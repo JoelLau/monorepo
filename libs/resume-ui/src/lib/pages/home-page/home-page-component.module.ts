b14920e992f453a259f4851da080d6fc03f438c9
Joel Lau <joel.lau@coinhako.com>
2023-03-01 07:29:55 +0800

    wip

--- libs/resume-ui/src/lib/pages/home-page/home-page-component.module.ts
+++ libs/resume-ui/src/lib/pages/home-page/home-page-component.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { HomePageRoutingModule } from './home-page-routing.module';
 import { HomePageComponent } from './home-page.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [HomePageComponent],
   exports: [HomePageComponent],
-  imports: [HomePageRoutingModule],
+  imports: [
+    // Routing
+    HomePageRoutingModule,
+
+    // Third Party
+    FontAwesomeModule,
+  ],
 })
 export class HomePageComponentModule {}
--- libs/resume-ui/src/lib/pages/home-page/home-page.component.ts
+++ libs/resume-ui/src/lib/pages/home-page/home-page.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'monorepo-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['home-page.component.scss'],
 })
-export class HomePageComponent {}
+export class HomePageComponent {
+  // icons
+  faLinkedin = faLinkedin;
+}
